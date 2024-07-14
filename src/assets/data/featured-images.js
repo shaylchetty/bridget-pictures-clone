f1ad79722583c1c83087ee074e3e55879d5da35a
Shayl Chetty <102982612+shaylchetty@users.noreply.github.com>
2024-07-14 16:32:31 -0400

    Update featured-images.js

--- src/assets/data/featured-images.js
+++ src/assets/data/featured-images.js
@@ -1,44 +1,40 @@
-import img1 from '../images/featured/1.jpg';
-import img2 from '../images/featured/2.jpg';
-import img3 from '../images/featured/3.jpg';
-import img4 from '../images/featured/4.jpg';
-import img5 from '../images/featured/5.jpg';
-// import img6 from '../images/featured/6.jpeg';
-// import img7 from '../images/featured/7.jpeg';
-// import img8 from '../images/featured/8.jpeg';
-// import img9 from '../images/featured/9.jpeg';
-// import img10 from '../images/featured/10.jpeg';
-// import img11 from '../images/featured/11.jpeg';
-// import img12 from '../images/featured/12.jpeg';
-// import img13 from '../images/featured/13.jpeg';
-// import img14 from '../images/featured/14.jpeg';
-// import img15 from '../images/featured/15.jpeg';
-// import img16 from '../images/featured/16.jpeg';
-// import img17 from '../images/featured/17.jpeg';
-// import img18 from '../images/featured/18.jpeg';
-// import img19 from '../images/featured/19.jpeg';
-// import img20 from '../images/featured/20.jpeg';
-// import img21 from '../images/featured/21.jpeg';
-// import img22 from '../images/featured/22.jpeg';
-// import img23 from '../images/featured/23.jpeg';
-// import img24 from '../images/featured/24.jpeg';
-// import img25 from '../images/featured/25.jpeg';
-// import img26 from '../images/featured/26.jpeg';
-// import img27 from '../images/featured/27.jpeg';
-// import img28 from '../images/featured/28.jpeg';
-// import img29 from '../images/featured/29.jpeg';
-// import img30 from '../images/featured/30.jpeg';
-// import img31 from '../images/featured/31.jpeg';
-// import img32 from '../images/featured/32.jpeg';
-// import img33 from '../images/featured/33.jpeg';
-// import img34 from '../images/featured/34.jpeg';
-// import img35 from '../images/featured/35.jpeg';
-// import img36 from '../images/featured/36.jpeg';
-// import img37 from '../images/featured/37.jpeg';
-// import img38 from '../images/featured/38.jpeg';
-// import img39 from '../images/featured/39.jpeg';
-// import img40 from '../images/featured/40.jpeg';
-// import img41 from '../images/featured/41.jpeg';
+import img1 from '../images/featured/1.JPEG';
+import img2 from '../images/featured/2.JPEG';
+import img3 from '../images/featured/3.JPEG';
+import img4 from '../images/featured/4.JPEG';
+import img5 from '../images/featured/5.JPEG';
+import img6 from '../images/featured/6.JPEG';
+import img7 from '../images/featured/7.JPEG';
+import img8 from '../images/featured/8.JPEG';
+import img9 from '../images/featured/9.JPEG';
+import img10 from '../images/featured/10.JPEG';
+import img11 from '../images/featured/11.JPEG';
+import img12 from '../images/featured/12.JPEG';
+import img13 from '../images/featured/13.JPEG';
+import img14 from '../images/featured/14.JPEG';
+import img15 from '../images/featured/15.JPEG';
+import img16 from '../images/featured/16.JPEG';
+import img17 from '../images/featured/17.JPEG';
+import img18 from '../images/featured/18.JPEG';
+import img19 from '../images/featured/19.JPEG';
+import img20 from '../images/featured/20.JPEG';
+import img21 from '../images/featured/21.JPEG';
+import img22 from '../images/featured/22.JPEG';
+import img23 from '../images/featured/23.JPEG';
+import img24 from '../images/featured/24.JPEG';
+import img25 from '../images/featured/25.JPEG';
+import img26 from '../images/featured/26.JPEG';
+import img27 from '../images/featured/27.JPEG';
+import img28 from '../images/featured/28.JPEG';
+import img29 from '../images/featured/29.JPEG';
+import img30 from '../images/featured/30.JPEG';
+import img31 from '../images/featured/31.JPEG';
+import img32 from '../images/featured/32.JPEG';
+import img33 from '../images/featured/33.JPEG';
+import img34 from '../images/featured/34.JPEG';
+import img35 from '../images/featured/35.JPEG';
+
+
 
 export const featuredImages = [
     img1,
@@ -46,40 +42,34 @@ export const featuredImages = [
     img3,
     img4,
     img5,
-    // img6,
-    // img7,
-    // img8,
-    // img9,
-    // img10,
-    // img11,
-    // img12,
-    // img13,
-    // img14,
-    // img15,
-    // img16,
-    // img17,
-    // img18,
-    // img19,
-    // img20,
-    // img21,
-    // img22,
-    // img23,
-    // img24,
-    // img25,
-    // img26,
-    // img27,
-    // img28,
-    // img29,
-    // img30,
-    // img31,
-    // img32,
-    // img33,
-    // img34,
-    // img35,
-    // img36,
-    // img37,
-    // img38,
-    // img39,
-    // img40,
-    // img41,
+    img6,
+    img7,
+    img8,
+    img9,
+    img10,
+    img11,
+    img12,
+    img13,
+    img14,
+    img15,
+    img16,
+    img17,
+    img18,
+    img19,
+    img20,
+    img21,
+    img22,
+    img23,
+    img24,
+    img25,
+    img26,
+    img27,
+    img28,
+    img29,
+    img30,
+    img31,
+    img32,
+    img33,
+    img34,
+    img35,
 ];
